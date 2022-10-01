f5dcb6a84f1c1bf6a865e08b6405fd85682c173a
PacFan29 <ksw2270274@stu.o-hara.ac.jp>
2022-10-01 14:27:01 +0900

    水の判定をボックス判定に

--- Assets/Gameplays/Stage/Gimmicks/Scripts/Common/UnderwaterManager.cs
+++ Assets/Gameplays/Stage/Gimmicks/Scripts/Common/UnderwaterManager.cs
@@ -5,10 +5,16 @@ using UnityEngine;
 public class UnderwaterManager : MonoBehaviour
 {
     public GameObject splashEffect;
-    void OnTriggerStay(Collider other) {
+    void OnTriggerEnter(Collider other) {
         if (other.gameObject.tag == "Player") {
             PlayerInfo player = other.GetComponent<PlayerInfo>();
-            player.underwater = (player.transform.position.y < this.transform.position.y);
+            player.underwaterTrigger = true;
+        }
+    }
+    void OnTriggerExit(Collider other) {
+        if (other.gameObject.tag == "Player") {
+            PlayerInfo player = other.GetComponent<PlayerInfo>();
+            player.underwaterTrigger = false;
         }
     }
 }
